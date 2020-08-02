8673fa2bde3f0e74716d373b499070b065b0e07e
nahummor <nahummor@gmail.com>
2020-08-02 23:18:35 +0300

    starting login page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { PageLayout } from '../common';
 const Home = () => {
-   return <PageLayout>Home</PageLayout>;
+   return (
+      <PageLayout>
+         <h1>Home</h1>
+         <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem
+            temporibus a saepe impedit quos nam odio nobis sed, illum tempore
+            atque provident quas, veniam consectetur ducimus eveniet vel
+            obcaecati. Magni?
+         </p>
+      </PageLayout>
+   );
 };
 
 export default Home;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import { PageLayout } from '../common/PageLayout';
+import React, { useState } from 'react';
+import { PageLayout, Input } from '../common';
+import styled from 'styled-components';
+
+const Form = styled.form`
+   width: 100%;
+   max-width: 400px;
+   background: white;
+   border: 1px solid #eee;
+   padding: 16px;
+   box-sizing: border-box;
+   color: black;
+   border-radius: 4px;
+`;
 
 const Login = () => {
-   return <PageLayout>Login</PageLayout>;
+   const [formFields, setFormFields] = useState({ userName: '', password: '' });
+
+   const handelInputChange = (event) => {
+      event.persist();
+
+      setFormFields((oldState) => ({
+         ...oldState,
+         [event.target.name]: event.target.value,
+      }));
+   };
+
+   return (
+      <PageLayout>
+         <h1>Login</h1>
+         <Form>
+            <Input
+               value={formFields.userName}
+               onChange={handelInputChange}
+               type='text'
+               name='userName'
+               placeholder='User Name'
+            />
+            <Input
+               value={formFields.password}
+               onChange={handelInputChange}
+               type='password'
+               name='password'
+               placeholder='Password'
+            />
+         </Form>
+      </PageLayout>
+   );
 };
 
 export default Login;
