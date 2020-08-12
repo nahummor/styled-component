443be27987e3cde993ec0f0f67c5ea053587b821
nahummor <nahummor@gmail.com>
2020-08-12 06:44:47 +0300

    adding page buttons on planets component

--- src/components/pages/StarWors/Planets.js
+++ src/components/pages/StarWors/Planets.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { PageLayout } from '../../common';
@@ -11,13 +12,51 @@ const Container = styled.div`
    align-items: center;
 `;
 
-const fetchPlanets = async () => {
-   const res = await fetch('https://swapi.dev/api/planets/');
+const ButtonsRow = styled.div`
+   display: flex;
+   flex-direction: row;
+   gap: 2rem;
+   padding: 1rem;
+   margin-bottom: 1rem;
+   background-color: #b7b7b7;
+   border-radius: 5px;
+   > button {
+      box-shadow: 0px 1px 0px 0px #f0f7fa;
+      background: linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
+      background-color: #33bdef;
+      border-radius: 6px;
+      border: 1px solid #057fd0;
+      display: inline-block;
+      cursor: pointer;
+      color: #ffffff;
+      font-family: Arial;
+      font-size: 15px;
+      font-weight: bold;
+      padding: 6px 24px;
+      text-decoration: none;
+      text-shadow: 0px -1px 0px #5b6178;
+      outline: none;
+   }
+
+   > button:hover {
+      background: linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
+      background-color: #019ad2;
+   }
+
+   > button:active {
+      position: relative;
+      top: 1px;
+   }
+`;
+
+const fetchPlanets = async (key, page) => {
+   const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
    return res.json();
 };
 
 const Planets = () => {
-   const { data, status } = useQuery('planets', fetchPlanets, {
+   const [page, setPage] = useState(1);
+   const { data, status } = useQuery(['planets', page], fetchPlanets, {
       staleTime: 2000,
       refetchOnWindowFocus: false,
       onSuccess: () => console.log('Data fetch with no problem..'),
@@ -29,6 +68,11 @@ const Planets = () => {
          <Container>
             <h1>Star Wars Info</h1>
             <h2>Planets</h2>
+            <ButtonsRow>
+               <button onClick={() => setPage(1)}>Page 1</button>
+               <button onClick={() => setPage(2)}>Page 2</button>
+               <button onClick={() => setPage(3)}>Page 3</button>
+            </ButtonsRow>
             {status === 'loading' && <div>Loading Data....</div>}
             {status === 'error' && <div>Error fetch data</div>}
             {status === 'success' && (
